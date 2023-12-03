7773530a618516fe1d09772aa0b28c96d8c9dea9
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2023-12-03 18:59:09 +0300

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import Hero from "./Hero";
+
+// We are importing all the componnets then exporting them
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Testimonials,
+    Footer,
+    Hero
+};
